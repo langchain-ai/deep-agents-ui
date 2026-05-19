a17fb91aff9057738b83f6dcb5bd5d99215b9ca7
ToTheos-Dev <ToTheos-Dev 97577314+ToTheos-Dev@users.noreply.github.com>
2026-05-19 18:41:54 +1000

    test: clean up failing tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
   transformIgnorePatterns: [
     "/node_modules/(?!(@testing-library|react-markdown|remark-|unified|unist-|mdast-|micromark|devlop|hast-|vfile-|trough-|bail|is-plain-obj|ccount|comma-separated-tokens|decode-named-character-reference|escape-string-regexp|github-slugger|html-url-attributes|property-information|space-separated-tokens|stringify-entities|web-namespaces|xtend)/)"
   ],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.cjs"],
   moduleDirectories: ["node_modules", "<rootDir>"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
